f26520026ae994858f220438bd259b4ee003a05c
ilsergeev <ilay199640@gmail.com>
2016-10-11 21:33:49 +0300

    Update lang_frequency.py

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,16 @@
-
+# -*- coding: utf-8 -*- 
 
 def load_data(filepath):
-    pass
-
+    with open(filepath,'r') as file:
+     data = file.read() 
+     file.close()
+    return data
+   
 
 def get_most_frequent_words(text):
     pass
 
 
 if __name__ == '__main__':
-    pass
+    filepath = '/projects/5_lang_frequency/tst_text.txt'
+    print (load_data(filepath))
